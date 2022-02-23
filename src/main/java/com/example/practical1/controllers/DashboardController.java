b5a8f272c26bc33573ebdcc49369107ed8b3a625
Ayana Adilova <aadilova19@gmail.com>
2022-02-23 12:45:50 +0600

    lab 3

--- src/main/java/com/example/practical1/controllers/DashboardController.java
+++ src/main/java/com/example/practical1/controllers/DashboardController.java
@@ -1,5 +1,6 @@
 package com.example.practical1.controllers;
 
+import com.example.practical1.models.CatalogQueryDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
